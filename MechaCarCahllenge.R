a8a57ba0ccafdec957503d53085a6c35c0069107
dasavage1412 <77762219+dasavage1412@users.noreply.github.com>
2021-05-23 02:23:10 -0700

    Add files via upload

--- MechaCarCahllenge.R
+++ MechaCarCahllenge.R
@@ -4,3 +4,11 @@ mpg_mod <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cle
 View(mpg_mod)
 mpg_mod_summary <- summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=Mecha_table))
 View(mpg_mod_summary)
+
+Suspension_Table <- read.csv(file= 'suspension_Coil.csv', check.names = F,stringsAsFactors = F)
+summarise(Suspension_Table,3)
+#Creates a summary stats column for the PSI COL
+total_summary <- Suspension_Table %>% group_by(PSI) %>% summarize(Mean=mean(PSI_col),median=median(PSI_col),variance= var(PSI_col), SD = sd(PSI_col), .groups = 'keep')
+
+# Create summary stats by lot
+lot_summary <- Suspension_Table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),median=median(PSI),variance= var(PSI), SD = sd(PSI), .groups = 'keep')
